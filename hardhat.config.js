6c598a0519f4bcb8fac2c77bc6ac6b8d96830d9a
starldev <>
2021-07-22 02:07:43 +0900

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +19,41 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.6.12",
+  solidity: {
+    version: "0.6.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://mainnet.infura.io/v3/3849a711ff6443d0b44b62f4156c7c0a",
+      },
+      chainId: 1337,
+    },
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/3849a711ff6443d0b44b62f4156c7c0a",
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ],
+    },
+    mainnet: {
+      url: "https://mainnet.infura.io/v3/3849a711ff6443d0b44b62f4156c7c0a",
+      accounts: [
+        process.env.PRIVATE_KEY,
+      ],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+  },
 };
